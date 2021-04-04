51996a9891d76838367fd2c958073d94ddc7317b
Dandy Ling <dandyling@gmail.com>
2021-04-04 21:53:44 +0800

    feat: Change checkbox to brand color

--- frontend/src/components/Checkbox/Checkbox.tsx
+++ frontend/src/components/Checkbox/Checkbox.tsx
@@ -9,7 +9,7 @@ export const Checkbox = (props: Props) => {
     <input
       type="checkbox"
       checked={checked}
-      className="w-5 h-5 mr-2 border-2 border-gray-400 rounded-sm focus:ring-transparent text-accent"
+      className="w-5 h-5 mr-2 border-2 border-gray-400 rounded-sm focus:ring-transparent text-brand"
       onChange={onChange}
     />
   );
